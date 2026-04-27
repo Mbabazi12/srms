d110fadda48a0fc5653aecd41d908b985d0d11d9
Mbabazi12 <mbabazi069@gmail.com>
2026-04-27 11:21:11 +0200

    db connection

---
+++ netlify/functions/getData.ts
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.DATABASE_URI;
+
+exports.handler = async () => {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const db = client.db("srms");
+
+    const data = await db.collection("srms").find().toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error instanceof Error ? error.message : String(error),
+    };
+  } finally {
+    await client.close();
+  }
+};
